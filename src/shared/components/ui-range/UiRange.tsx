c9561016c08e99c6d112f1e9bd1fad67840a78d0
Sergey <serjioini@gmail.com>
2019-07-16 01:47:37 +0300

    UiRange is functional component

--- src/shared/components/ui-range/UiRange.tsx
+++ src/shared/components/ui-range/UiRange.tsx
@@ -6,56 +6,54 @@ import 'rc-slider/assets/index.css';
 export interface IProps extends RangeProps {
 }
 
-class UiRange extends React.Component<IProps> {
-  render() {
-    const handle = (props) => {
-      const {value, dragging, index, ...restProps} = props;
-      return (
-        <Tooltip
-          prefixCls="rc-slider-tooltip"
-          overlay={value}
-          visible={dragging}
-          placement="top"
-          key={index}
-        >
-          <Handle value={value} {...restProps} />
-        </Tooltip>
-      );
-    };
-    const handleStyle = [
-      {
-        borderColor: '#2684FF',
-        backgroundColor: 'white'
-      },
-      {
-        borderColor: '#2684FF',
-        backgroundColor: 'white'
-      }
-    ];
-    const dotStyle = {
-      backgroundColor: '',
-      borderColor: 'hsl(0, 0%, 80%)',
-    };
-    const railStyle = {
-      backgroundColor: 'hsl(0, 0%, 80%)',
-      height: '4px',
-    };
-    const trackStyle = [{
-      backgroundColor: '#2684FF',
-    }];
-
+const UiRange: React.FC<IProps> = (props) => {
+  const handle = (props) => {
+    const {value, dragging, index, ...restProps} = props;
     return (
-      <Range
-        {...this.props}
-        allowCross={false}
-        handle={handle}
-        handleStyle={handleStyle}
-        dotStyle={dotStyle}
-        railStyle={railStyle}
-        trackStyle={trackStyle}
-      />
+      <Tooltip
+        prefixCls="rc-slider-tooltip"
+        overlay={value}
+        visible={dragging}
+        placement="top"
+        key={index}
+      >
+        <Handle value={value} {...restProps} />
+      </Tooltip>
     );
-  }
-}
+  };
+  const handleStyle = [
+    {
+      borderColor: '#2684FF',
+      backgroundColor: 'white'
+    },
+    {
+      borderColor: '#2684FF',
+      backgroundColor: 'white'
+    }
+  ];
+  const dotStyle = {
+    backgroundColor: '',
+    borderColor: 'hsl(0, 0%, 80%)',
+  };
+  const railStyle = {
+    backgroundColor: 'hsl(0, 0%, 80%)',
+    height: '4px',
+  };
+  const trackStyle = [{
+    backgroundColor: '#2684FF',
+  }];
+
+  return (
+    <Range
+      {...props}
+      allowCross={false}
+      handle={handle}
+      handleStyle={handleStyle}
+      dotStyle={dotStyle}
+      railStyle={railStyle}
+      trackStyle={trackStyle}
+    />
+  );
+};
 
 export default UiRange;
